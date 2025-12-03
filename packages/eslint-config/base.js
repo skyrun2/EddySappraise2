08d576bcc7f02f55ddc2ff537c145e89b83aad3d
Edwin Divine <omegbuedwin@gmail.com>
2025-12-03 08:05:41 +0100

    Track apps folder structure

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -3,7 +3,7 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
 import onlyWarn from "eslint-plugin-only-warn";
-
+         
 /**
  * A shared ESLint configuration for the repository.
  *
